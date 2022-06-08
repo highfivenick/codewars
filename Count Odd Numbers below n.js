dc2f70e84989aa16e4f1a02d374634d82b98e3bb
nick <101999951+highfivenick@users.noreply.github.com>
2022-06-08 16:37:40 -0400

    Create Count Odd Numbers below n.js
    Given a number n, return the number of positive odd numbers below n
    
    Examples (Input -> Output)
    7  -> 3 (because odd numbers below 7 are [1, 3, 5])
    15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])

---
+++ Count Odd Numbers below n.js
@@ -0,0 +1,9 @@
+function oddCount(n){
+  // your code here
+  return Math.floor(n/2)
+}
+
+//Parameters - n is a number
+//Return - a number represeting the number of odd numbers that are less than n
+//Example - 28 would return 14
+//Pseduocode - half the numbers under any given number would be odd, so i can divide by 2 and round down to account for odd numbers
