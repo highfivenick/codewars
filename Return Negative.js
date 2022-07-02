bafd2a3990fe52d4966a837a288903fce6d35958
nick <101999951+highfivenick@users.noreply.github.com>
2022-07-01 23:22:51 -0400

    Create Return Negative.js
    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
    
    Examples
    
    makeNegative(1);    // return -1
    makeNegative(-5);   // return -5
    makeNegative(0);    // return 0
    makeNegative(0.12); // return -0.12
    Notes
    
    The number can be negative already, in which case no change is required.
    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.

---
+++ Return Negative.js
@@ -0,0 +1,15 @@
+function makeNegative(num) {
+  // Code?
+  let str = num.toString()
+  if (str === '0' || (str.charAt(0) === '-')){
+    return num
+  }else {
+    let neg = `-${str}`
+    return Number(neg)
+  }
+}
+
+//Parameters - num is a number
+//Return - return the argument as a negative
+//Example - 7 would return -7, 0 would return 0, -.1971 would return -.1971
+//Pseudocode - check if the number is positive, negative, or 0; if positive, return its negative, if 0 or negative, return num
